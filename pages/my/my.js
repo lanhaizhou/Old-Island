7425ee64f541cc51f21e9a2ef02e81f48780fb0d
lulu <1398668823@qq.com>
2020-02-18 14:36:52 +0800

    ”我的“页面，ending

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,20 +1,83 @@
-// pages/my/my.js
+import {
+  ClassicModel
+} from '../../models/classic.js'
+import{
+  BookModel
+} from '../../models/book.js'
+let classicModel = new ClassicModel();
+let bookModel = new BookModel();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    authorized:false,
+    userInfo:null,
+    bookCount:0,
+    classics:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.userAuthorized();
+    this.getMyBookCount();
+    this.getMyFavor();
   },
-
+  getMyFavor(){
+    classicModel.getMyFavor(res=>{
+      console.log(res)
+      this.setData({
+        classics:res
+      })
+    })
+  },
+  getMyBookCount(){
+    bookModel.getMyBookCount().then((res) =>{
+      this.setData({
+        bookCount:res.count
+      })
+    })
+  },
+  userAuthorized(){
+    wx.getSetting({
+      success:data => {
+        console.log(data)
+        if(data.authSetting['scope.userInfo']){
+          wx.getUserInfo({
+            success:data => {
+              //console.log(data)
+              this.setData({
+                authorized:true,
+                userInfo:data.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+  onGetUserInfo(event){
+    const userInfo = event.detail.userInfo;
+    if (userInfo){
+      this.setData({
+        userInfo,
+        authorized: true
+      })
+    }
+  },
+  onJumpToAbout(event){
+    wx.navigateTo({
+      url: '/pages/about/about',
+    })
+  },
+  onStudy(){
+    wx.navigateTo({
+      url: '/pages/course/course',
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
